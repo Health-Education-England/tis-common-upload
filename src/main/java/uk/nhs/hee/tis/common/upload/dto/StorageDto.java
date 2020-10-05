8163a7548c2773b3b2df57e43f21491a21495248
JosephKelly <joseph.kelly@hee.nhs.uk>
2020-10-05 11:50:08 +0100

    feat: Include metadata to persist with files
    Save any metadata defined by the client, e.g. "lifecycleState" of a form
    
    TISNEW-5376: Enable TIS Admins to see submitted Form Rs

--- src/main/java/uk/nhs/hee/tis/common/upload/dto/StorageDto.java
+++ src/main/java/uk/nhs/hee/tis/common/upload/dto/StorageDto.java
@@ -2,6 +2,7 @@ package uk.nhs.hee.tis.common.upload.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import java.util.List;
+import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -18,6 +19,7 @@ public class StorageDto {
   private String bucketName;
   private String folderPath;
   private String key;
+  private Map<String, String> customMetadata;
   private List<MultipartFile> files;
 
 }
--- src/test/java/uk/nhs/hee/tis/common/upload/controller/AwsStorageControllerTest.java
+++ src/test/java/uk/nhs/hee/tis/common/upload/controller/AwsStorageControllerTest.java
@@ -21,10 +21,12 @@
 
 package uk.nhs.hee.tis.common.upload.controller;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -97,8 +99,15 @@ public class AwsStorageControllerTest {
     mockMvc.perform(multipart(STORAGE_URL + UPLOAD)
         .file(file)
         .param("bucketName", bucketName)
-        .param("folderPath", folderPath))
+        .param("folderPath", folderPath)
+        .param("customMetadata[key]", "value")
+        .param("customMetadata[uploadDate]", "1992-08-07"))
         .andExpect(status().isOk());
+
+    verify(storageService).upload(storageDtoCaptor.capture());
+    StorageDto expectedDto = StorageDto.builder().bucketName(bucketName).folderPath(folderPath)
+        .files(List.of(file)).customMetadata(Map.of("key", "value", "uploadDate", "1992-08-07")).build();
+    assertThat(storageDtoCaptor.getValue(), equalTo(expectedDto));
   }
 
   @Test
